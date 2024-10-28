1656656a5bc89ef2ad9ac611f384f519a76ad5a1
WhoamI00 <ankitisop12@gmail.com>
2024-10-28 19:23:19 +0530

    Server to Client

--- app/page.jsx
+++ app/page.jsx
@@ -1,109 +1,20 @@
-"use client";
-import { useState, useEffect } from "react";
-import PromptCard from "@components/PromptCard";
+"use client"
+import Feed from "@components/Feed";
 
-const PromptCardList = ({ data, handleTagClick }) => {
-  return (
-    <div className='mt-16 prompt_layout'>
-      {data.map((post) => (
-        <PromptCard
-          key={post._id}
-          post={post}
-          handleTagClick={handleTagClick}
-        />
-      ))}
-    </div>
-  );
-};
-
-const Home = () => {
-  const [allPosts, setAllPosts] = useState([]);
-  const [searchText, setSearchText] = useState("");
-  const [searchTimeout, setSearchTimeout] = useState(null);
-  const [searchedResults, setSearchedResults] = useState([]);
-
-  const fetchPosts = async () => {
-    try {
-      const response = await fetch("/api/prompt");
-      const data = await response.json();
-      setAllPosts(data);
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
-  useEffect(() => {
-    if (!searchText) {
-      setSearchedResults([]);
-    }
-  }, [searchText]);
-
-  const filterPrompts = (searchtext) => {
-    const regex = new RegExp(searchtext, "i");
-    return allPosts.filter(
-      (item) =>
-        regex.test(item.creator.username) ||
-        regex.test(item.tag) ||
-        regex.test(item.prompt)
-    );
-  };
-
-  const handleSearchChange = (e) => {
-    clearTimeout(searchTimeout);
-    setSearchText(e.target.value);
-    setSearchTimeout(
-      setTimeout(() => {
-        const searchResult = filterPrompts(e.target.value);
-        setSearchedResults(searchResult);
-      }, 500)
-    );
-  };
-
-  const handleTagClick = (tagName) => {
-    setSearchText(tagName);
-    const searchResult = filterPrompts(tagName);
-    setSearchedResults(searchResult);
-  };
-
-  return ( // Moved the return statement inside the Home component
-    <section className='w-full flex-center flex-col'>
-      <h1 className='head_text text-center'>
-        Discover & Share
-        <br className='max-md:hidden' />
-        <span className='orange_gradient text-center'> AI-Powered Prompts</span>
-      </h1>
-      <p className='desc text-center'>
-        PromptPal is an open-source AI prompting tool for modern world to
-        discover, create and share creative prompts
-      </p>
-      <section className='feed'>
-        <form className='relative w-full flex-center'>
-          <input
-            type='text'
-            placeholder='Search for a tag or a username'
-            value={searchText}
-            onChange={handleSearchChange}
-            required
-            className='search_input peer'
-          />
-        </form>
-
-        {/* All Prompts */}
-        {searchText ? (
-          <PromptCardList
-            data={searchedResults}
-            handleTagClick={handleTagClick}
-          />
-        ) : (
-          <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
-        )}
-      </section>
-    </section>
-  );
-};
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+      Discover & Share
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+    </h1>
+    <p className='desc text-center'>
+      PromptPal is an open-source AI prompting tool for modern world to
+      discover, create and share creative prompts
+    </p>
+    
+    <Feed />
+  </section>
+);
 
 export default Home;
